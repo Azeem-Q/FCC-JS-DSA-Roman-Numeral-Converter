f0d79f90b3caa19bb476348f63c4d691c6f2effa
Azeem <livelyazeem@gmail.com>
2022-03-30 19:52:11 +0500

    in process

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 // Roman Numeral Converter
 
-console.log(Math.floor(3549/1000));
+
+function convertToRoman(num) {
+    let romanObj = {
+        1: 'I',
+        4: 'IV',
+        5: 'V',
+        9: 'IX',
+        10: 'X',
+        40: 'XL',
+        50: 'L',
+        90: 'XC',
+        100: 'C',
+        400: 'CD',
+        500: 'D',
+        900: 'CM',
+        1000: 'M'
+    }
+    let keyArr = Object.keys(romanObj);
+    let init = 12;
+    while (i > 0) {
+        
+    }
+}
+
+console.log(convertToRoman(30));
+//console.log(Math.floor(3549/1000));
