19b47d472b5288f655ede749a90149d7ad71905c
Azeem <livelyazeem@gmail.com>
2022-03-31 12:23:45 +0500

    made arrays of ints and roman nums

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 
 function convertToRoman(num) {
+    let ints = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+    let romanNum = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
     let romanObj = {
         1: 'I',
         4: 'IV',
@@ -19,8 +21,8 @@ function convertToRoman(num) {
     }
     let keyArr = Object.keys(romanObj);
     let init = 12;
-    while (i > 0) {
-        
+    while (num > 0) {
+                
     }
 }
 
