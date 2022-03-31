ea13cb09918099b775c64219ba5b054e04fa2c10
Azeem <livelyazeem@gmail.com>
2022-03-31 12:45:14 +0500

    constructing while loop

--- script.js
+++ script.js
@@ -4,9 +4,12 @@
 function convertToRoman(num) {
     let ints = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
     let romanNum = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
-    let init = 12;
     while (num > 0) {
-                
+        for (let i = ints.length - 1; i > 0; i--) {
+            if (ints[i] > num) {
+                let div = num / ints[i];
+            }
+        }    
     }
 }
 
