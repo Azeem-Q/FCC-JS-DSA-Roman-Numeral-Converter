ad90a238bf017e0c2c5a1a32ae86fbcc000ef479
Azeem <livelyazeem@gmail.com>
2022-03-31 12:56:54 +0500

    still constructing while loop

--- script.js
+++ script.js
@@ -4,13 +4,17 @@
 function convertToRoman(num) {
     let ints = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
     let romanNum = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
+    let finalArr = [];
     while (num > 0) {
         for (let i = ints.length - 1; i > 0; i--) {
             if (ints[i] > num) {
-                let div = num / ints[i];
+                let div = Math.floor(num / ints[i]);
+                num = num % ints[i];
+                finalArr.push(romanNum[i].repeat(div));
             }
         }    
     }
+    //console.log(romanNum[0].repeat(3));
 }
 
 console.log(convertToRoman(30));
